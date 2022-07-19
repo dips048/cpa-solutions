7e5729c8f7ebc84a16ca8957b77930506cedf1d5
dips048 <dipeshpatel048@gmail.com>
2022-07-19 23:13:51 +0530

    home page modifications

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,40 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { gsap, Sine } from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { filter, map, Observable } from 'rxjs';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { filter, Observable } from 'rxjs';
 import { DataService } from '../../services';
 import { NavigationListModel } from '../../models';
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
   resourceNavList$: Observable<NavigationListModel[]>;
 
-  constructor(
-    private dataService: DataService
-  ) {
-    this.resourceNavList$ = this.dataService.getNavigationList().pipe(
-      filter((navList, i) => navList[i].title === 'Resources'),
-    );
-   }
+  constructor(private dataService: DataService) {
+    this.resourceNavList$ = this.dataService
+      .getNavigationList()
+      .pipe(filter((navList, i) => navList[i].title === 'Resources'));
+  }
 
   ngOnInit(): void {
     this.initAnimation();
   }
 
   initAnimation() {
-    gsap.from('#container-2', {
+    gsap.from('#nl-container', {
       scrollTrigger: {
-        trigger: "#container-2",
-        endTrigger: "#container-3",
-        end: "bottom 50%+=100px",
-      }, opacity: 0, y: -200, duration: 1});
+        trigger: '#nl-container',
+      },
+      opacity: 0,
+      y: -200,
+      duration: 2,
+    });
+    gsap.from('.footer-card', {
+      scrollTrigger: {
+        trigger: '.footer-card',
+      },
+      opacity: 0,
+      scale: 0,
+      duration: 2,
+    });
   }
 }
--- src/app/components/nav-list/nav-list.component.ts
+++ src/app/components/nav-list/nav-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { NavigationListModel } from 'src/app/models';
+import { NavigationListModel } from '../../models';
 import { DataService } from '../../services';
 
 @Component({
