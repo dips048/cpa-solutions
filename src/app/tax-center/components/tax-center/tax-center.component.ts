148f46eccf1bf7d8040dc34e15360dba8a0220f1
dips048 <dipeshpatel048@gmail.com>
2022-07-20 16:40:13 +0530

    modification in tax-center component

--- src/app/tax-center/components/tax-center/tax-center.component.ts
+++ src/app/tax-center/components/tax-center/tax-center.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { gsap } from 'gsap';
+import { ContactUsDialogComponent } from '../../../shared/components/contact-us-dialog/contact-us-dialog.component';
 
 @Component({
   selector: 'app-tax-center',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaxCenterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.initAnimation();
   }
 
+  initAnimation(): void {
+    gsap.from("#header-1",{y: -100, opacity: 0, duration: 1})
+  };
+
+  openContactUsDialog() {
+    this.dialog.open(ContactUsDialogComponent, {
+      width: '500px'
+    });
+  };
 }
--- src/app/tax-center/tax-center.module.ts
+++ src/app/tax-center/tax-center.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { TaxCenterRoutingModule } from './tax-center-routing.module';
 import { components } from './components';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { components } from './components';
   ],
   imports: [
     CommonModule,
-    TaxCenterRoutingModule
+    TaxCenterRoutingModule,
+    MatButtonModule,
   ]
 })
 export class TaxCenterModule { }
