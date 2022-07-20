577e10b44491f88ad380b3231f4b82799d2f1a8d
dips048 <dipeshpatel048@gmail.com>
2022-07-20 16:38:46 +0530

    modification in contact component

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { components } from './components';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { components } from './components';
   ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    MatButtonModule
   ]
 })
 export class ContactModule { }
