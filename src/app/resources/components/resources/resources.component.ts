e2fe44eaf27a41ba8bff83dd1f87f96e78aaeff9
dips048 <dipeshpatel048@gmail.com>
2022-07-20 16:39:23 +0530

    modification in resources component

--- src/app/resources/components/resources/resources.component.ts
+++ src/app/resources/components/resources/resources.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { gsap } from 'gsap';
+import { ContactUsDialogComponent } from '../../../shared/components/contact-us-dialog/contact-us-dialog.component';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.css']
+  styleUrls: ['./resources.component.css'],
 })
 export class ResourcesComponent implements OnInit {
-
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.initAnimation();
   }
 
+  initAnimation(): void {
+    gsap.from('#header-1', { y: -100, opacity: 0, duration: 1 });
+  }
+
+  openContactUsDialog() {
+    this.dialog.open(ContactUsDialogComponent, {
+      width: '500px',
+    });
+  }
 }
