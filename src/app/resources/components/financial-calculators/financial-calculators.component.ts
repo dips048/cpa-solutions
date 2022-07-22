856287ff9f2feae78ff909e042e46de0f1be4664
dips048 <dipeshpatel048@gmail.com>
2022-07-23 00:08:10 +0530

    + animation to finanicial calculators

--- src/app/resources/components/financial-calculators/financial-calculators.component.ts
+++ src/app/resources/components/financial-calculators/financial-calculators.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ContactUsDialogComponent } from '../../../shared/components/contact-us-dialog/contact-us-dialog.component';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-financial-calculators',
@@ -14,10 +18,20 @@ export class FinancialCalculatorsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.initAnimation();
   }
 
   initAnimation(): void {
-
+    gsap.to('#image-container-2', {
+      scrollTrigger: {
+        trigger: '#image-container-1',
+        start: "top top",
+        end: "bottom 50%+=140px",
+        pin: true,
+        markers: true,
+        pinSpacing: false
+      }
+    });
   };
 
   openContactUsDialog() {
