6f61b6ec96dc94dd0c788fc0d67b6a64ddbf8dc1
dips048 <dipeshpatel048@gmail.com>
2022-07-25 17:44:43 +0530

    + created deatail form component

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { gsap } from 'gsap';
 import { MatDialog } from '@angular/material/dialog';
 import { ContactUsDialogComponent } from '../../../shared/components/contact-us-dialog/contact-us-dialog.component';
-import ScrollTrigger from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -17,11 +18,20 @@ export class ContactComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.initAnimation();
+    this.initAnimation();
   }
 
   initAnimation(): void {
-    
+    gsap.to('#image-container-2', {
+      scrollTrigger: {
+        trigger: '#image-container-1',
+        start: "top top",
+        end: "bottom 50%+=140px",
+        pin: true,
+        markers: true,
+        pinSpacing: false
+      }
+    });
   };
 
   openContactUsDialog() {
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,10 +2,12 @@ import { ContactUsDialogComponent } from "./contact-us-dialog/contact-us-dialog.
 import { LoginDialogComponent } from "./login-dialog/login-dialog.component";
 import { SubscribeDialogComponent } from "./subscribe-dialog/subscribe-dialog.component";
 import { HeadingComponent } from "./heading/heading.component";
+import { DetailFormComponent } from "./detail-form/detail-form.component";
 
 export const components = [
   LoginDialogComponent,
   SubscribeDialogComponent,
   ContactUsDialogComponent,
   HeadingComponent,
+  DetailFormComponent,
 ];
