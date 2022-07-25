40ff6bdff9eebccc732b204b74db1ffd27f156d0
dips048 <dipeshpatel048@gmail.com>
2022-07-25 17:46:23 +0530

    + animation & nav in service for individuals comp

--- src/app/service/components/service-for-individuals/service-for-individuals.component.ts
+++ src/app/service/components/service-for-individuals/service-for-individuals.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ContactUsDialogComponent } from '../../../shared/components/contact-us-dialog/contact-us-dialog.component';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-service-for-individuals',
@@ -18,8 +22,18 @@ export class ServiceForIndividualsComponent implements OnInit {
   }
 
   initAnimation(): void {
+    gsap.to('#image-container-2', {
+      scrollTrigger: {
+        trigger: '#image-container-1',
+        start: "top top",
+        end: "bottom 50%+=140px",
+        pin: true,
+        markers: true,
+        pinSpacing: false
+      }
+    });
+  };
 
-  }
 
   openContactUsDialog() {
     this.dialog.open(ContactUsDialogComponent, {
